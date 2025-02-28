f5568666c117a2a49f2475681fc86416dde8964d
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2025-02-28 16:40:42 +0200

    Move name generation logic into a struct

--- src/lib.rs
+++ src/lib.rs
@@ -5,67 +5,117 @@ extern crate alloc;
 
 use rand::Rng;
 
-const ANIMALS: &[&str] = &const_str::split!(include_str!("data/en_animals.txt"), "\n");
-const TOTAL_ANIMALS: usize = ANIMALS.len();
-const ADJECTIVES: &[&str] = &const_str::split!(include_str!("data/en_adjectives.txt"), "\n");
-const TOTAL_ADJECTIVES: usize = ADJECTIVES.len();
+pub const DEFAULT_GOOFY_ANIMALS: GoofyAnimals<'static> = GoofyAnimals::new(
+    &const_str::split!(include_str!("data/en_animals.txt"), "\n"),
+    &const_str::split!(include_str!("data/en_adjectives.txt"), "\n"),
+);
 
-const _: () = {
-    if const_str::equal!(*ANIMALS.last().unwrap(), "") {
-        panic!("trailing newline in animals");
-    }
+pub struct GoofyAnimals<'a> {
+    animals: &'a [&'a str],
+    adjectives: &'a [&'a str],
+}
 
-    if const_str::equal!(*ADJECTIVES.last().unwrap(), "") {
-        panic!("trailing newline in adjectives");
-    }
-};
+impl<'a> GoofyAnimals<'a> {
+    pub const fn new(animals: &'a [&'a str], adjectives: &'a [&'a str]) -> Self {
+        let total_animals = animals.len();
+        let total_adjectives = adjectives.len();
 
-#[cfg_attr(feature = "tracing", tracing::instrument(skip(rng), level = tracing::Level::TRACE))]
-pub fn generate_name_parts(rng: &mut impl Rng) -> (&str, &str, &str) {
-    let (adjective_one, adjective_two) = loop {
-        let one = rng.gen_range(0..TOTAL_ADJECTIVES);
-        let two = rng.gen_range(0..TOTAL_ADJECTIVES);
-
-        if one == two {
-            continue;
+        if total_animals < 1 {
+            panic!("empty animals");
         }
 
-        break (one, two);
-    };
+        if total_adjectives < 2 {
+            panic!("must have at least two adjectives");
+        }
 
-    let animal = rng.gen_range(0..TOTAL_ANIMALS);
+        if const_str::equal!(*animals.last().unwrap(), "") {
+            panic!("trailing newline in animals");
+        }
 
-    #[cfg(feature = "tracing")]
-    tracing::trace!(adjective_one, adjective_two, animal, "generated name");
+        if const_str::equal!(*adjectives.last().unwrap(), "") {
+            panic!("trailing newline in adjectives");
+        }
 
-    (
-        ADJECTIVES[adjective_one],
-        ADJECTIVES[adjective_two],
-        ANIMALS[animal],
-    )
+        Self {
+            animals,
+            adjectives,
+        }
+    }
+
+    pub fn new_unchecked(animals: &'a [&'a str], adjectives: &'a [&'a str]) -> Self {
+        Self {
+            animals,
+            adjectives,
+        }
+    }
+
+    pub fn get_animals(&self) -> &'a [&'a str] {
+        self.animals
+    }
+
+    pub fn get_adjectives(&self) -> &'a [&'a str] {
+        self.adjectives
+    }
+
+    #[cfg_attr(feature = "tracing", tracing::instrument(skip(rng), level = tracing::Level::TRACE))]
+    pub fn generate_name_parts(&self, rng: &mut impl Rng) -> (&'a str, &'a str, &'a str) {
+        let (adjective_one, adjective_two) = loop {
+            let one = rng.gen_range(0..self.adjectives.len());
+            let two = rng.gen_range(0..self.adjectives.len());
+
+            if one == two {
+                continue;
+            }
+
+            break (one, two);
+        };
+
+        let animal = rng.gen_range(0..self.animals.len());
+
+        #[cfg(feature = "tracing")]
+        tracing::trace!(adjective_one, adjective_two, animal, "generated name");
+
+        (
+            self.adjectives[adjective_one],
+            self.adjectives[adjective_two],
+            self.animals[animal],
+        )
+    }
+
+    #[inline]
+    #[cfg(feature = "alloc")]
+    pub fn generate_name(&self, rng: &mut impl Rng) -> ::alloc::string::String {
+        let (adjective_one, adjective_two, animal) = self.generate_name_parts(rng);
+
+        ::alloc::format!("{adjective_one}-{adjective_two}-{animal}")
+    }
+}
+
+#[inline]
+pub fn generate_name_parts(rng: &mut impl Rng) -> (&'static str, &'static str, &'static str) {
+    DEFAULT_GOOFY_ANIMALS.generate_name_parts(rng)
 }
 
 #[inline]
 #[cfg(feature = "alloc")]
 pub fn generate_name(rng: &mut impl Rng) -> ::alloc::string::String {
-    let (adjective_one, adjective_two, animal) = generate_name_parts(rng);
-
-    ::alloc::format!("{adjective_one}-{adjective_two}-{animal}")
+    DEFAULT_GOOFY_ANIMALS.generate_name(rng)
 }
 
 #[cfg(test)]
 mod test {
-    use super::{TOTAL_ADJECTIVES, TOTAL_ANIMALS};
+    use super::DEFAULT_GOOFY_ANIMALS;
+
     use pretty_assertions::assert_eq;
 
     #[test]
     fn animals() {
-        assert_eq!(TOTAL_ANIMALS, 355);
+        assert_eq!(DEFAULT_GOOFY_ANIMALS.get_animals().len(), 355);
     }
 
     #[test]
     fn adjectives() {
-        assert_eq!(TOTAL_ADJECTIVES, 1300);
+        assert_eq!(DEFAULT_GOOFY_ANIMALS.get_adjectives().len(), 1300);
     }
 
     #[test]
@@ -74,40 +124,38 @@ mod test {
         use rand::SeedableRng;
         use rand_chacha::ChaCha20Rng;
 
-        use super::generate_name_parts;
-
         let mut rng = ChaCha20Rng::seed_from_u64(0x1337);
 
         assert_eq!(
-            generate_name_parts(&mut rng),
+            DEFAULT_GOOFY_ANIMALS.generate_name_parts(&mut rng),
             ("healthy", "frivolous", "dove"),
         );
         assert_eq!(
-            generate_name_parts(&mut rng),
+            DEFAULT_GOOFY_ANIMALS.generate_name_parts(&mut rng),
             ("glorious", "meager", "polar bear"),
         );
         assert_eq!(
-            generate_name_parts(&mut rng),
+            DEFAULT_GOOFY_ANIMALS.generate_name_parts(&mut rng),
             ("thankful", "elastic", "clownfish"),
         );
         assert_eq!(
-            generate_name_parts(&mut rng),
+            DEFAULT_GOOFY_ANIMALS.generate_name_parts(&mut rng),
             ("vigilant", "troubled", "firefly"),
         );
         assert_eq!(
-            generate_name_parts(&mut rng),
+            DEFAULT_GOOFY_ANIMALS.generate_name_parts(&mut rng),
             ("handsome", "modest", "porcupine"),
         );
         assert_eq!(
-            generate_name_parts(&mut rng),
+            DEFAULT_GOOFY_ANIMALS.generate_name_parts(&mut rng),
             ("sunny", "wonderful", "dormouse"),
         );
         assert_eq!(
-            generate_name_parts(&mut rng),
+            DEFAULT_GOOFY_ANIMALS.generate_name_parts(&mut rng),
             ("treasured", "woozy", "deer"),
         );
         assert_eq!(
-            generate_name_parts(&mut rng),
+            DEFAULT_GOOFY_ANIMALS.generate_name_parts(&mut rng),
             ("mealy", "cylindrical", "dog"),
         );
 
@@ -134,10 +182,11 @@ mod test {
         use rand::SeedableRng;
         use rand_chacha::ChaCha20Rng;
 
-        use super::generate_name;
-
         let mut rng = ChaCha20Rng::seed_from_u64(0x1337);
 
-        assert_eq!(generate_name(&mut rng), "healthy-frivolous-dove",);
+        assert_eq!(
+            DEFAULT_GOOFY_ANIMALS.generate_name(&mut rng),
+            "healthy-frivolous-dove",
+        );
     }
 }
